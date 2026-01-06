d2a3c3b2d03c2a185c35cc72ce18ea8d620fc831
Nicolas Fréret <contact.nicolasfreret@gmail.com>
2026-01-06 17:11:11 +0100

    help

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import './assets/css/style.scss';
 
+(function(){
+
+    const header = document?.querySelector('header'),
+    nav = header?.querySelector('nav'),
+    hbg = header?.querySelector('.hbg')
+
+    if(hbg){
+        hbg.addEventListener('click', e=>{
+            e.preventDefault()
+            let res = header.classList.toggle('open')
+            nav.setAttribute('aria-expanded', res)
+        })
+    }
 
 
+})()
